4ca692a1ea3a853e7ed59c85f1a3f2cc4ec9b13e
roshandakhorevnitnagpur <roshandakhore.vnitnagpur@gmail.com>
2026-08-21 20:48:30 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 3.6
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Roshan"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
