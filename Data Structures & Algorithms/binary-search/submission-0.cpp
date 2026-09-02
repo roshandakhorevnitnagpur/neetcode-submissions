7a82dec62f9094a8a107622e4a1a063f587b924a
roshandakhorevnitnagpur <roshandakhore.vnitnagpur@gmail.com>
2026-09-02 11:31:41 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int returnIndex(vector<int>& nums,int target){
+        int start = 0;
+        int end= nums.size()-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] == target){
+                ans =  mid;
+                break;
+            }
+            else if(nums[mid] > target){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+    int search(vector<int>& nums, int target) {
+        int index = returnIndex(nums,target);
+        return index;
+    }
+};
