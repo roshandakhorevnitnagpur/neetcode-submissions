2966713c7740128500869118455153ae4a4688c3
Roshan Dakhore <roshandakhore.vnitnagpur@gmail.com>
2026-09-18 09:50:24 +0530

    Add: sql-evaluate-boolean-expression - submission-0

---
+++ SQL/sql-evaluate-boolean-expression/submission-0.sql
@@ -0,0 +1,14 @@
+-- Write your query below
+SELECT 
+    e.left_operand,
+    e.operator,
+    e.right_operand,
+    CASE 
+        WHEN e.operator = '>' AND lv.value > rv.value THEN 'true'
+        WHEN e.operator = '<' AND lv.value < rv.value THEN 'true'
+        WHEN e.operator = '=' AND lv.value = rv.value THEN 'true'
+        ELSE 'false'
+    END AS value
+FROM expressions e
+JOIN variables lv ON lv.name = e.left_operand
+JOIN variables rv ON rv.name = e.right_operand
